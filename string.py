d645945f54bb973003b8f9612e18e18d763accc9
Your name here <brkmer@gmail.com>
2022-07-06 17:06:51 -0400

    "string operators push"

---
+++ string.py
@@ -0,0 +1,8 @@
+course = "python for begin"
+print(course)
+print(course.upper())
+print(course.find('h'))
+print(course.find('H'))
+print(course.find('y'))
+print(course.replace('for', '4'))
+print('python' in course)
