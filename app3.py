28989ca48a9873d14f4fe3ed701b4f114302672d
Your name here <brkmer@gmail.com>
2022-07-06 13:14:12 -0400

    "first push"

---
+++ app3.py
@@ -0,0 +1,10 @@
+first = input("first number?" )
+second = input ("second number?" )
+calculate = float(first) + float(second)
+print("sum: " + str(calculate))
+
+
+first = float(input("first number?" ))
+second = float(input ("second number?" ))
+calculate = first + float
+print("sum: " + str(calculate))
