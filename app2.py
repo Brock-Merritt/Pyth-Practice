a32ae431dd1efba6d3869d5f7ea8d8590c69a282
Your name here <brkmer@gmail.com>
2022-07-06 13:20:38 -0400

    "app 2 push"

---
+++ app2.py
@@ -0,0 +1,13 @@
+name = input("oi whats ya name? ")
+print("hello " + name)
+
+
+birth_year = input("enter birth year: ")
+age = 2020 - int(birth_year)
+print(age)
+
+
+int()
+float()
+bool()
+str()
