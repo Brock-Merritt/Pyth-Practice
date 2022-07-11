b55d7fddc26dbc4c43d6afe482b6c4788a8fd357
Your name here <brkmer@gmail.com>
2022-07-11 14:53:55 -0400

    "Logical Operators"

---
+++ Logical Operators.py
@@ -0,0 +1,13 @@
+price = 50
+print(price > 10 and price < 30)
+
+price2 = 26
+print(price2 > 10 or price <30)
+
+
+price3 = 5
+print(not price3 > 10)
+
+# and (both)
+# or (at least one)
+# not (inverse )
