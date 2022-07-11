c0d6b33fb9550a7697cd7b5039824287a5341a71
Your name here <brkmer@gmail.com>
2022-07-11 18:04:51 -0400

    "if statements"

---
+++ if Statements.py
@@ -0,0 +1,13 @@
+temperature = 5
+
+if temperature > 30:
+    print("its hot out")
+    print("drink water")
+elif temperature > 20:
+    print("its nice out")
+elif temperature > 10:
+    print("its cold")
+else:
+    print("its really cold")
+print("done")
+
