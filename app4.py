d2e87a6797cf90f2def770f45d683493c01da070
Your name here <brkmer@gmail.com>
2022-07-13 18:48:58 -0400

    "app 4- weight converter"

---
+++ app4.py
@@ -0,0 +1,13 @@
+# first = input("weight:")
+# second = input(" kg or lbs:")
+#
+# print( "weight in " + (second) + ":" + (first) )
+
+weight = int(input("weight:"))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = weight /0.45
+    print("Weight in Lbs: " + converted)
+else:
+    converted = weight *0.45
+    print("Weight in Kgs: " + converted)
