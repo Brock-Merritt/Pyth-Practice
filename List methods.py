1179b206ff209b67fdc8e4c5557833304ad8c0f8
Your name here <brkmer@gmail.com>
2022-07-18 17:45:22 -0400

    "List methods"

---
+++ List methods.py
@@ -0,0 +1,21 @@
+number = [1, 2, 3 ,4, 5]
+number.append(6)
+print(number)
+
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(0, -1)
+print(numbers)
+
+numbers1 = [1, 2, 3, 4, 5]
+numbers1.remove(3)
+print(numbers1)
+
+numbers = [1, 2, 3, 4, 5]
+numbers.clear()
+print(numbers)
+
+numbers2 = [1, 2, 3, 4, 5]
+print(1 in numbers2)
+
+numbers3 = [1, 2, 3, 4, 5]
+print(len(numbers))
