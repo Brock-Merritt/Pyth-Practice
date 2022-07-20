038ee620c54e0d3570ffbeed7ee79925ec9b29c9
Your name here <brkmer@gmail.com>
2022-07-20 15:15:32 -0400

    "Range() functions"

---
+++ range() function.py
@@ -0,0 +1,17 @@
+numbers = range(5)
+for number in numbers:
+    print(number)
+
+
+numbers = range(10, 15)
+for number in numbers:
+    print(number)
+
+
+numbers = range(20, 30, 2)
+for number in numbers:
+    print(number)
+
+for number in range(5):
+    print(number)
+
